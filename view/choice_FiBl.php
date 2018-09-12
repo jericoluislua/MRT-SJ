0b2ef42ca3fa66314ac7fef67637d6aa006298d5
Sivakeerthan Vamanarajasekaran <keerthan_1@hotmail.com>
2018-09-12 17:10:02 +0200

    training-views are ready to be edited

---
+++ view/choice_FiBl.php
@@ -0,0 +1,32 @@
+<?php if(!isset($_SESSION)){session_start();} if(isset($_SESSION['uid'])):?>
+</div>
+<div class="choice">
+    <div id="profile" class="card-panel">
+        <h1 ><?=$uname?></h1>
+        <img src="/images/placeholders/profile-picture.png" class="profile-picture" alt="profile-picture">
+    </div>
+    <div id="training-canvas" class="card">
+    <div id="FiBl">
+        <div id="img-div">
+            <img src="/images/placeholders/FiBl_placeholder.jpg" class="code-image" alt="code-image"/>
+        </div>
+        <div id="answer-div">
+        <form>
+        <div class="form-group input-field col s6">
+          <input id="last_name" type="text" class="validate">
+          <label for="last_name">Last Name</label>
+          <button id="send" name="send" type="submit" class="btn btn-primary">
+            <i class="material-icons">send</i>
+          </button>
+        </div>
+         </form>
+        </div>
+    </div>
+    </div>
+</div>
+
+<div class="container">
+    <?php endif;?>
+    <?php if(!isset($_SESSION['uid'])):?>
+        <h1>Access Denied!</h1>
+<?php endif;?>
--- view/header.php
+++ view/header.php
@@ -7,12 +7,10 @@
 
     <title><?= $title ?> | IDPA BWD</title>
 
-    <!-- Custom styles for this template -->
+
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-      <!--Import Google Icon Font-->
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <!--Import materialize.css-->
-      <link type="text/css" rel="stylesheet" href="/materialize/css/materialize.min.css"  media="screen,projection"/>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="/materialize/css/materialize.min.css"  media="screen,projection"/>
     <link href="/css/main.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
