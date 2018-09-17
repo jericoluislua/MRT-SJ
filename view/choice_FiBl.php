e0d0920f2a7941eb04593975bc905a06fb1df519
Sivakeerthan Vamanarajasekaran <keerthan_1@hotmail.com>
2018-09-17 14:13:30 +0200

    Prototype ready

--- view/choice_FiBl.php
+++ view/choice_FiBl.php
@@ -14,7 +14,7 @@
         <form>
         <div class="form-group input-field col s6">
           <input id="last_name" type="text" class="validate">
-          <label for="last_name">Last Name</label>
+          <label for="last_name">Answer</label>
           <button id="send" name="send" type="submit" class="btn btn-primary">
             <i class="material-icons">send</i>
           </button>
