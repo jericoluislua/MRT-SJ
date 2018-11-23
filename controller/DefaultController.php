07cd5ecda7fa21b15078195aed03e9cdd0d88d1c
s.vamanarajasekaran <s.vamanarajasekaran@iet-gibb.ch>
2018-11-23 21:20:05 +0100

    images fixed

--- controller/DefaultController.php
+++ controller/DefaultController.php
@@ -37,8 +37,8 @@ class DefaultController
         //   "default_index" rendern. Wie das genau funktioniert, ist in der
         //   View Klasse beschrieben.
         $view = new View('default_index');
-        $view->title = 'Startseite';
-        $view->heading = 'Startseite';
+        $view->title = 'Home';
+        $view->heading = 'Home';
         $view->display();
     }
 }
